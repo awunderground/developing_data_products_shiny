dd798e57e7ae806f5e02fcf65581284982251683
awunderground <aaronrwilliams100@gmail.com>
2018-08-27 15:28:04 -0400

    Add plot cluster centers.

--- app.R
+++ app.R
@@ -14,10 +14,14 @@ ui <- fluidPage(
            sliderInput(inputId = "seasons", label = "Seasons", 
                        min = 2010, max = 2016, value = c(2000, 2016), step = 1),
            sliderInput(inputId = "clusters", label = "Number of Clusters", 
-                       min = 2, max = 20, value = 4, step = 1),           
+                       min = 2, max = 10, value = 3, step = 1),           
            radioButtons("distance", "Distribution type:",
                         c("Euclidean" = "euclidean",
-                          "Manhattan" = "manhattan"))
+                          "Manhattan" = "manhattan")),
+           radioButtons("scale", "Type of scaling",
+                        c("Raw" = "raw",
+                          "Linear" = "linear",
+                          "Standardized" = "standardized"))
     )
   )
 )
@@ -29,34 +33,59 @@ server <- function(input, output) {
     # filter the data based on user options for seasons
     players_subset <-
       players %>%
-      filter(season >= input$seasons[1] & season <= input$seasons[2]) %>%
-      select(season, last_name, first_name, average_minutes, usage)
+      filter(season >= input$seasons[1] & season <= input$seasons[2])
     
     # drop player names
     players_numeric <- 
       players_subset %>%
       select_if(is.numeric)
     
+    # scale data
+    if (input$scale == "raw") {
+      # do nothing
+    } else if (input$scale == "linear") {
+      players_numeric <- players_numeric %>%
+        mutate_all(scale)   
+    } else if (input$scale == "standardize") {
+      players_numeric <- players_numeric %>%
+        mutate_all(stats::rescale, to = c(1, 100))      
+    }
+
+    # set seed for kmeans starting points
     set.seed(20180826)
     
+    # fit kmeans model
     clusters <- kmeans(players_numeric, centers = input$clusters, nstart = input$clusters)
     
-    print(augment(clusters, players_subset))
+    # create a tibble of data points
+    tidy_clusters <- augment(clusters, players_subset)
     
-    output$plot <- renderPlot({ augment(clusters, players_subset) %>%
-      ggplot(aes(average_minutes, usage, color = .cluster)) +
-      geom_point(alpha = 0.5, size = 3)
-    })
+    # perform principal components analysis on scaled data
+    pc <- prcomp(players_numeric, scale. = FALSE)
+    
+    # project clusters onto the pca space
+    tidy_centers <- predict(pc, clusters[["centers"]]) %>%
+      as_tibble() %>%
+      mutate(.cluster = as.character(row_number()))
+    
+    print(tidy_centers)
+    
+    # create plot
+    pc_data <- pc %>%
+      .[["x"]] %>%
+      as_tibble()
+    
+    tidy_clusters <- bind_cols(tidy_clusters, pc_data)
+        
+    ggplot() +
+      geom_point(data = tidy_clusters, 
+                 aes(PC1, PC2, color = .cluster), 
+                 alpha = 0.5, size = 3) +
+      geom_point(data = tidy_centers, 
+                 aes(PC1, PC2, fill = .cluster), 
+                 alpha = 0.8, size = 5, shape = 24, color = "black", stroke = 2)
       
-    print(prcomp(players_numeric, scale. = TRUE))
-    
   })
 }
 
 shinyApp(ui = ui, server = server)
-
-
-
-
-
-# ??kmeans
